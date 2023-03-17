73a90f78e7ced9031b1b890d801361ec1181b434
Scott <saseaa37@gmail.com>
2023-03-16 21:08:19 -0600

    FSDI-112-S2

---
+++ posts/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Post(models.Model):                                   # inheritance
+    title = models.CharField(max_length=256)                # compostition
+    subtitle = models.CharField(max_length=256)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
